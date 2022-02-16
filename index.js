237def800cbb56d9ea442b0e5b97922191013054
Deepshikha Mishra <>
2022-02-16 11:46:21 +0530

    2

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function calculate()
 { 
     c(eval(document.getElementById("result").value))  
 } 
+function clicked(){
+    console.log('eqwe')
+    // let text=document.createElement('div')
+    // text.innerHTML += `<p>hello</p>`
+    // document.getElementById('abc').appendChild(text)
 
+    var table= document.createElement('Table')
+    var tableBody = document.createElement('tbody')
+    table.appendChild(tableBody)
+    var count = 1; 
+    for(var i =0; i< 3;i++){
+        var tr = document.createElement('tr')
+        tableBody.appendChild(tr)
+        for(var j = 0; j< 3;j++){
+            var td = document.createElement('td')
+            td.appendChild(document.createTextNode(count++));
+            tr.appendChild(td)
+        }
+    }
+    document.getElementById('a').appendChild(table)
+}
